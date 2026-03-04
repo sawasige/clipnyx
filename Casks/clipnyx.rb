baa62b009546a3492f0ca98ac135d3cbaaca5e17
SAWADA Shigeru <6521997+sawasige@users.noreply.github.com>
2026-03-04 11:36:22 +0900

    Homebrew CaskのURLをsawasige orgに修正しsed置換を改善

--- Casks/clipnyx.rb
+++ Casks/clipnyx.rb
@@ -2,10 +2,10 @@ cask "clipnyx" do
   version "1.0.0"
   sha256 "5bcfe8293d3f4c1a1ccc68111b548fdd90fb951f8fe7e8e19710faac72e4e113"
 
-  url "https://github.com/sawada-sh/clipboard-mac/releases/download/v#{version}/Clipnyx.dmg"
+  url "https://github.com/sawasige/clipboard-mac/releases/download/v#{version}/Clipnyx.dmg"
   name "Clipnyx"
   desc "Clipboard history manager for macOS menu bar"
-  homepage "https://github.com/sawada-sh/clipboard-mac"
+  homepage "https://github.com/sawasige/clipboard-mac"
 
   depends_on macos: ">= :sequoia"
 
